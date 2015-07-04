600a59a6995a16d6766abff219ce38c2b39dff4d
AKostilenko <okostylenko@malkosua.com>
2015-07-04 18:07:17 +0300

    another one

--- ContosoUniversity/remotes/master2nd.cs
+++ ContosoUniversity/remotes/master2nd.cs
@@ -5,7 +5,8 @@ using System.Web;
 
 namespace ContosoUniversity.remotes
 {
-    public class Class2
+    public class master2nd
     {
+        //commit after 65670fffeec6398758020711838a30fa8010d941
     }
 }
