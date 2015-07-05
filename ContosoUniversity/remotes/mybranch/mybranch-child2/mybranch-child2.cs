5b7e5cd8a4e4e3921c696f4e99536302bf384afa
AKostilenko <okostylenko@malkosua.com>
2015-07-05 18:42:19 +0300

    okostilenko mybranch after 325673e0adebc3a8c33fbd908574c15bd2e7db91
    okostilenko mybranch-child2 after 2e2fc8c2eac68e9daedc5d0fae87ea067f9dc9b3
    
    okostilenko mybranch-child2 after 28b6e1e7430d9eedf6bb968d9f07cb9665291a9d

---
+++ ContosoUniversity/remotes/mybranch/mybranch-child2/mybranch-child2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.remotes.mybranch.mybranch_child2
+{
+    public class mybranch_child2
+    {
+        //okostilenko mybranch-child2 after 2e2fc8c2eac68e9daedc5d0fae87ea067f9dc9b3
+        //okostilenko mybranch-child2 after 28b6e1e7430d9eedf6bb968d9f07cb9665291a9d
+
+    }
+}
--- ContosoUniversity/remotes/mybranch/mybranch.cs
+++ ContosoUniversity/remotes/mybranch/mybranch.cs
@@ -10,7 +10,7 @@ namespace ContosoUniversity
         //okostilenko mybranch after b31a801dfe49d429201d06bc36b125407b89dd28
         //okostilenko mybranch after f09537182c80002eca8241966e5aed4fb742ff32
         //okostilenko mybranch after 2bfced2e28e54b2946acb979d4baa9f21f8ee846
-        //okostilenko mybranch after b0b8ac594350e59b28dd197b8d10fdb0c30ac7ee
-
+        //okostilenko mybranch after 0a928b53744d335b23711e172436ffd3b9cfc6ec
+        //okostilenko mybranch after 325673e0adebc3a8c33fbd908574c15bd2e7db91
     }
 }
