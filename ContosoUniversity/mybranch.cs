4069eea32ee94ef115d45d41fcc949f8ddfee9ed
AKostilenko <okostylenko@malkosua.com>
2015-07-05 18:07:51 +0300

    okostilenko mybranch after b31a801dfe49d429201d06bc36b125407b89dd28

---
+++ ContosoUniversity/mybranch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity
+{
+    public class mybranch
+    {
+        //okostilenko mybranch after b31a801dfe49d429201d06bc36b125407b89dd28
+    }
+}
