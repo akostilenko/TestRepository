93112b6067702907542d4515efb8676128856746
AKostilenko <okostylenko@malkosua.com>
2015-07-05 18:18:22 +0300

    okostilenko mybranch_child1 after a7791d3e8aaa154b77150badabcfde0defad66f0

---
+++ ContosoUniversity/remotes/mybranch-child1/mybranch-child1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.remotes
+{
+    public class mybranch_child1
+    {
+        //okostilenko mybranch_child1 after a7791d3e8aaa154b77150badabcfde0defad66f0
+    }
+}
